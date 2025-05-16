85cffb199b8768f618bc7f0a91214bb5f9316f14
hayleyrnaidoo <hayleynaidoo17@gmail.com>
2025-05-16 17:42:53 +0200

    Initial commit

--- obj/Debug/net8.0/catalog.AssemblyInfo.cs
+++ obj/Debug/net8.0/catalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5c8f692770724db34a826e3b6bf48f3d7feb93")]
 [assembly: System.Reflection.AssemblyProductAttribute("catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
